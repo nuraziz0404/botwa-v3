b822dbce82e18e64cc8144d6b4a66df41384806d
Akbarsans <77180979+Akbarsans@users.noreply.github.com>
2021-01-09 21:22:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -119,8 +119,8 @@ conn.handler = async function (m) {
 
 conn.on('message-new', conn.handler) 
 conn.on('error', conn.logger.error)
-global.mods = []
-global.prems = []
+global.mods = ['6285890216240@s.whatsapp.net']
+global.prems = ['6285890216240@s.whatsapp.net']
 
 global.dfail = (type, m, conn) => {
   let msg = {
