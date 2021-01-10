dcd2cab0f8f76d1936f8a9c5ce73451663252a20
Akbarsans <77180979+Akbarsans@users.noreply.github.com>
2021-01-10 11:55:48 +0700

    Update info.js

--- plugins/info.js
+++ plugins/info.js
@@ -1,24 +1,22 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
   conn.reply(m.chat, `
-╠═〘 INFO BOT 〙 ═
-╠➥ Dibuat dengan bahasa javascript via NodeJs
-╠➥ Rec: Drawl Nag
-╠➥ Script: @Akbarsans
-║
-╠➥ Github: https://github.com/Akbarsans/Miray-chan
-╠➥ Instagram: @arpunchs
-╠➥ YouTube: Drawl Nag
-║
-╠═〘 Thanks To 〙 ═
-╠➥ Allah SWT
-║
-╠═〘 DONASI 〙 ═
-╠➥ pulsa : 0838-7733-5693
-╠➥ dana: 0858-9021-6240
-╠➥ ovo: 0858-9021-6240
-║
-║
-╠═〘 Miray-chan 〙 ═
+〘 INFO BOT 〙
+Dibuat dengan bahasa javascript via NodeJs
+
+➥ Github: https://github.com/Akbarsans/Miray-chan
+➥ Instagram: instagram.com/akbarsan3
+➥ YouTube: aa akbar
+
+〘 Thanks To 〙 
+➥ Allah SWT
+
+〘 DONASI 〙 
+➥ pulsa : 0838-7733-5693
+➥ dana: 0858-9021-6240
+➥ ovo: 0858-9021-6240
+
+
+〘 Miray-chan 〙 
 `.trim(), m)
 }
 handler.command = /^(info)$/i
